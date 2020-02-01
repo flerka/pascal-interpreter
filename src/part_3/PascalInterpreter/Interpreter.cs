7bb71083190385f9e7bb08e04501dfb0415993e2
flerka <marya.kozhemyakina@gmail.com>
2020-02-01 10:01:19 +0200

    add part 3 base code

---
+++ src/part_3/PascalInterpreter/Interpreter.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace PascalInterpreter
+{
+    public class Interpreter
+    {
+        private readonly string _text;
+
+        private int position = 0;
+        private Token currentToken = null;
+        private char? currentChar;
+
+        /// <summary>
+        /// Token ctr
+        /// </summary>
+        /// <param name="text">string input, e.g. "3+5"</param>
+        public Interpreter(string text)
+        {
+            this._text = text;
+            currentChar = _text[0];
+        }
+
+        /// <summary>
+        /// Parser / Interpreter
+        /// expr -> INTEGER PLUS INTEGER
+        /// expr -> INTEGER MINUS INTEGER
+        /// </summary>
+        public int BuildExpression()
+        {
+            // set current token to the first token taken from the input
+            this.currentToken = GetNextToken();
+
+            // we expect the current token to be a single-digit integer
+            var left = this.currentToken;
+            this.Eat(TokenType.INTEGER);
+
+            // we expect the current token to be a '+' token
+            var op = this.currentToken;
+            if (op.Type != TokenType.PLUS && op.Type != TokenType.MINUS)
+            {
+                throw new Exception();
+            }
+            this.Eat(op.Type);
+
+            // we expect the current token to be a single-digit integer
+            var right = this.currentToken;
+            this.Eat(TokenType.INTEGER);
+
+            // at this point either the INTEGER PLUS INTEGER or
+            // the INTEGER MINUS INTEGER sequence of tokens
+            // has been successfully found and the method can just
+            // return the result of adding or subtracting two integers,
+            // thus effectively interpreting client input
+            var leftNumber = int.Parse(left.Value);
+            var rightNumber = int.Parse(right.Value);
+            return op.Type == TokenType.MINUS ? leftNumber - rightNumber : leftNumber + rightNumber; 
+        }
+
+        /// <summary>
+        /// Lexical analyzer (also known as scanner or tokenizer)
+        /// This method is responsible for breaking a sentence
+        /// apart into tokens. One token at a time.
+        /// </summary>
+        private Token GetNextToken()
+        {
+            while (currentChar != null)
+            {
+                // if the character is a digit then convert it to
+                // integer, create an INTEGER token, increment self.pos
+                // index to point to the next character after the digit,
+                // and return the INTEGER token
+                switch (currentChar)
+                {
+                    case ' ':
+                        this.SkipWhitespaces();
+                        continue;
+                    case '+':
+                        UpdatePosition();
+                        return new Token(TokenType.PLUS, currentChar.ToString());
+                    case '-':
+                        UpdatePosition();
+                        return new Token(TokenType.MINUS, currentChar.ToString());
+                    case var _ when int.TryParse(currentChar.ToString(), out int number):
+                        return new Token(TokenType.INTEGER, GetMultidigitIntSubstring());
+                    default:
+                        throw new Exception();
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Return a (multidigit) integer consumed from the input.
+        /// </summary>
+        private string GetMultidigitIntSubstring()
+        {
+            var result = string.Empty;
+            while(currentChar != null && char.IsDigit(currentChar.Value))
+            {
+                result += currentChar;
+                UpdatePosition();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Compare the current token type with the passed token
+        /// type and if they match then "eat" the current token
+        /// and assign the next token to the self.current_token,
+        /// otherwise raise an exception.
+        /// </summary>
+        private void Eat(TokenType tokenType)
+        {
+            if (this.currentToken.Type != tokenType)
+            {
+                throw new Exception();
+            }
+
+            this.currentToken = GetNextToken();
+        }
+
+        private void UpdatePosition()
+        {
+            position++;
+            if (position > _text.Length - 1)
+            {
+                currentChar = null;
+            }
+            else
+            {
+                currentChar = _text[position];
+            }
+        }
+
+        private void SkipWhitespaces()
+        {
+            while (this.currentChar != null && Char.IsWhiteSpace(this.currentChar.Value))
+            {
+                UpdatePosition();
+            }
+        }
+    }
+}
---
+++ src/part_3/PascalInterpreter/Token.cs
@@ -0,0 +1,22 @@
+﻿
+namespace PascalInterpreter
+{
+    public class Token
+    {
+        public TokenType Type { get; private set; }
+        public string Value { get; private set; }
+
+        /// <summary>
+        /// Interpreter ctr
+        /// </summary>
+        /// <param name="type">token value: 0, 1, 2. 3, 4, 5, 6, 7, 8, 9, '+', or None</param>
+        /// <param name="value">token type: INTEGER, PLUS, or EOF</param>
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public override string ToString() => $"{Type}, {Value}";
+    }
+}
