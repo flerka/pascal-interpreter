83a6736433589e9c3a188d3af9687e72def5ae30
flerka <marya.kozhemyakina@gmail.com>
2020-02-10 18:06:44 +0200

    add part 6 base code

---
+++ src/part_6/PascalInterpreter/Lexer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PascalInterpreter
+{
+    public class Lexer
+    {
+        private int _position;
+        private char? _currentChar;
+
+        private readonly string _text;
+
+        /// <param name="text">string input, e.g. "3+5"</param>
+        public Lexer(string text)
+        {
+            _text = text;
+            if (_text != null) _currentChar = _text[0];
+        }
+
+        /// <summary>
+        /// Lexical analyzer (also known as scanner or tokenizer)
+        /// This method is responsible for breaking a sentence
+        /// apart into tokens. One token at a time.
+        /// </summary>
+        public Token GetNextToken()
+        {
+            while (_currentChar != null)
+            {
+                // if the character is a digit then convert it to
+                // integer, create an INTEGER token, increment position
+                // index to point to the next character after the digit,
+                // and return the INTEGER token
+                switch (_currentChar)
+                {
+                    case ' ':
+                        SkipWhitespace();
+                        continue;
+                    case '*':
+                        Advance();
+                        return new Token(TokenType.MUL, _currentChar.ToString());
+                    case '/':
+                        Advance();
+                        return new Token(TokenType.DIV, _currentChar.ToString());
+                    case '+':
+                        Advance();
+                        return new Token(TokenType.PLUS, _currentChar.ToString());
+                    case '-':
+                        Advance();
+                        return new Token(TokenType.MINUS, _currentChar.ToString());
+                    case var _ when int.TryParse(_currentChar.ToString(), out _):
+                        return new Token(TokenType.INTEGER, Integer());
+                    default:
+                        throw new InvalidSyntaxException();
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Advance the position pointer and set the currentChar variable.
+        /// </summary>
+        private void Advance()
+        {
+            _position++;
+            if (_position > _text.Length - 1)
+            {
+                _currentChar = null;
+            }
+            else
+            {
+                _currentChar = _text[_position];
+            }
+        }
+
+        /// <summary>
+        /// Return a (multidigit) integer consumed from the input.
+        /// </summary>
+        private string Integer()
+        {
+            var result = string.Empty;
+            while (_currentChar != null && char.IsDigit(_currentChar.Value))
+            {
+                result += _currentChar;
+                Advance();
+            }
+
+            return result;
+        }
+
+        private void SkipWhitespace()
+        {
+            while (_currentChar != null && Char.IsWhiteSpace(_currentChar.Value))
+            {
+                Advance();
+            }
+        }
+    }
+}
---
+++ src/part_6/PascalInterpreter/Token.cs
@@ -0,0 +1,22 @@
+﻿
+namespace PascalInterpreter
+{
+    public class Token
+    {
+        public TokenType Type { get; }
+        public string Value { get; }
+
+        /// <summary>
+        /// Interpreter ctr
+        /// </summary>
+        /// <param name="type">token value: 0, 1, 2. 3, 4, 5, 6, 7, 8, 9, '+', or None</param>
+        /// <param name="value">token type: INTEGER, PLUS, MUL, DIV or EOF</param>
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public override string ToString() => $"{Type}, {Value}";
+    }
+}
